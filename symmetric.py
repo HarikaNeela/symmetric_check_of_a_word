e7b32eb2e50207c3693c7e8f812c14f54a35a315
Harika Neela <97505929+HarikaNeela@users.noreply.github.com>
2022-06-11 20:01:42 +0530

    Update symmetric.py

--- symmetric.py
+++ symmetric.py
@@ -1,11 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 mystring=input()
 n=len(mystring)
 mid=n/2
@@ -17,25 +9,13 @@ if n%2==0:
         l.append(mystring[x])
     for y in range(int(mid),n):
         m.append(mystring[y])
-'''
-print(l)
-print(m)
-
-for p in range(0):
-    if(l[p]==m[p]):
-        count+=1
-if count==mid:
-    print("symmetric")
-else:
-    print("non sym")
-        
-    '''
-if l==m:
+    if l==m:
         print("symmetric")
+    else:
+        print("non sym")
 else:
-    print("non sym")
+    print("non symmetric")
         
 
 
-    
         
